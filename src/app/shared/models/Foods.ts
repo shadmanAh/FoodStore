26fde7bbb30f6cf01f320610cd29f781af648894
Shadman <shadman061@gmail.com>
2023-03-31 12:03:27 +0430

    create header

---
+++ src/app/shared/models/Foods.ts
@@ -0,0 +1,11 @@
+export class Food{
+    id!:string;
+    name!:string;
+    price!:number;
+    tage?:string[];
+    favorite!:boolean;
+    stars!:number;
+    imageUrl!:string;
+    origins!:string[];
+    cookTime!:string;
+}
